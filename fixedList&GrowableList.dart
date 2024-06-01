057e968c503ce37dc1c8401885961789d747bcca
Mahabub Ul Hasib Shawon <96023468+mahabubulhasibshawon@users.noreply.github.com>
2024-06-01 22:23:07 +0600

    Create fixedList&GrowableList.dart

---
+++ fixedList&GrowableList.dart
@@ -0,0 +1,14 @@
+void main(){
+  var city = ['Dhaka','Rangpur','Khulna',10,true];  // declaring list
+  // printing the list
+  print(city);
+  // add a city and print
+  city.add('Chittagong');
+  print(city);
+
+//   making fixed list
+  const game = ['football', 'cricket'];
+  // game.add('golf');
+  print("printing games :");
+  print(game);
+}
