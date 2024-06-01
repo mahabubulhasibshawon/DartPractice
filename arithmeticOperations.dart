20a54e670e62e67afb10c1d39309a713a7f3244e
Mahabub Ul Hasib Shawon <96023468+mahabubulhasibshawon@users.noreply.github.com>
2024-06-01 16:55:30 +0600

    Create arithmeticOperations.dart

---
+++ arithmeticOperations.dart
@@ -0,0 +1,24 @@
+void main(){
+  // declaring variable , assigning value and perform Arithmatic operations
+  // declaring variable
+  var num1 = 40;
+  var num2 = 20;
+  // sum
+  var sum = num1+num2;
+  print(sum);
+  // sub
+  var sub = num1-num2;
+  print(sub);
+  // multiply
+  var multiply = num1*num2;
+  print(multiply);
+  // divide
+  var divide = num1/num2;
+  print(divide);
+  // divide int
+  var divide_int = num1~/num2;
+  print(divide_int);
+  // modolus
+  var modolus = num1%num2;
+  print(modolus);
+}
