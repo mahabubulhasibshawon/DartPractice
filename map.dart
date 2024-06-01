cb7f2b73ec39274e75fc856f58812170184b1928
Mahabub Ul Hasib Shawon <96023468+mahabubulhasibshawon@users.noreply.github.com>
2024-06-01 17:12:10 +0600

    Add files via upload

---
+++ map.dart
@@ -0,0 +1,11 @@
+void main(){
+  // create map
+  var student = {
+    'name': 'Hasib',
+    'age': 24,
+    'Dept': 'CSE'
+  };
+  print(student);
+//   print individual key
+  print(student['name']);
+}
