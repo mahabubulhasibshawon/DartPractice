88037d34aab78f3c218ef92beca0c82bf5d16667
Mahabub Ul Hasib Shawon <96023468+mahabubulhasibshawon@users.noreply.github.com>
2024-06-01 21:51:01 +0600

    create list properties

---
+++ listProperties.dart
@@ -0,0 +1,26 @@
+void main(){
+  var city = ['Dhaka','Rangpur','Khulna',10,true];  // declaring list
+//   calling first element
+  print(city.first);
+  // calling hashcode
+  print(city.hashCode);
+  // last entry
+  print(city.last);
+  // length
+  print(city.length);
+// reverse
+  print(city.reversed);
+  // cheking empty
+  print(city.isEmpty);
+  print(city.isNotEmpty);
+  // rumtime type
+  print(city.runtimeType);
+  // first or null
+  var citynull = [];
+  print(citynull.firstOrNull);
+
+// single
+  var vill = ['Ashulia'];
+  print(vill.single);
+
+}
