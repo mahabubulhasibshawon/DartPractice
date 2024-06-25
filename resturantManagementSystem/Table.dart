9f9d0334f659ea0767ebec7c51bc1d5e0c9d17bb
Mahabub Ul Hasib Shawon <96023468+mahabubulhasibshawon@users.noreply.github.com>
2024-06-25 11:32:37 +0600

    Update Table.dart

--- resturantManagementSystem/Table.dart
+++ resturantManagementSystem/Table.dart
@@ -1,3 +1,30 @@
-class Table{
+import 'Customer.dart';
+import 'Employee.dart';
+import 'Order.dart';
 
+class Table {
+  int tableNumber;
+  int capacity;
+  bool isReserved;
+  Employee? waitstaff;
+  Order? currentOrder;
+
+  Table(this.tableNumber, this.capacity,
+      [this.isReserved = false, this.waitstaff, this.currentOrder]);
+
+  void reserve(Customer customer) {
+    isReserved = true;
+    print('Table $tableNumber is reserver for Mr/Mrs ${customer.name}');
+  }
+
+  void assignWaitstaff(Employee employee) {
+    waitstaff = employee;
+    print('Waitstaff ${employee.name} assigned to table $tableNumber');
+  }
+
+  @override
+  String toString() {
+    return 'Table{tableNumber: $tableNumber, capacity: $capacity, isReserved: $isReserved, '
+        'waitstaff: ${waitstaff?.name}, currentOrder: ${currentOrder?.orderId}}';
+  }
 }
