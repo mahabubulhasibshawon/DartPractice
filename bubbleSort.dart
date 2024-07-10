98b0a64dfefef81a5ae69db6da637a5190d243b0
Mahabub Ul Hasib Shawon <96023468+mahabubulhasibshawon@users.noreply.github.com>
2024-07-11 00:15:06 +0600

    Update bubbleSort.dart

--- bubbleSort.dart
+++ bubbleSort.dart
@@ -1,27 +1,38 @@
+void bubbleSort(List<int> arr) {
+  int swapCount = 0; // Initialize swap counter
 
-  void bubbleSort(List<int> arr) {
-    for (int turn = 0; turn < arr.length - 1; turn++) {
-      for (int j = 0; j < arr.length - 1 - turn; j++) {
-        if (arr[j] > arr[j + 1]) {
-//           swap
-          int temp = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = temp;
-        }
+  for (int turn = 0; turn < arr.length - 1; turn++) {
+    for (int j = 0; j < arr.length - 1 - turn; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapCount++; // Increment the swap counter
       }
     }
-  }
-
-  void printArr(List<int> arr) {
-    for (int i = 0; i < arr.length; i++) {
-      print(arr[i]);
+    // If no swaps were made, the array is already sorted
+//     if (!swapped) {
+//       break;
+//     }
+    if (swapCount == 0) {
+      print("sorted already");
+      break;
     }
   }
 
-  void main() {
-    List<int> arr = [5, 4, 1, 3, 2];
-//   print(arr);
-    bubbleSort(arr);
-    printArr(arr);
-  }
+  print('Total swaps: $swapCount'); // Print the swap count
+}
 
+void printArr(List<int> arr) {
+  for (int i = 0; i < arr.length; i++) {
+    print(arr[i]);
+  }
+}
+
+void main() {
+  List<int> arr = [1, 2, 3, 4, 5];
+  // print(arr);
+  bubbleSort(arr);
+  printArr(arr);
+}
