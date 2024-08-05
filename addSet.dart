b9837c93adb8fbfccac9d616b855078c93dc1e7e
Mahabub Ul Hasib Shawon <96023468+mahabubulhasibshawon@users.noreply.github.com>
2024-08-05 23:55:16 +0600

    Update addSet.dart

--- addSet.dart
+++ addSet.dart
@@ -1,11 +1,14 @@
 void main(){
   // declare set
+  
   var myCitySet = <String>{'Dhaka', 'Barisal', 'CoxBazar'};
   print('Set = $myCitySet');
   // add
+  
   myCitySet.add('Khulna');
   print('Add a city in the Set = $myCitySet');
   // add all 
+  
   myCitySet.addAll({"Chittagong", "Rajshahi"});
   print('Add multiple city in the Set = $myCitySet');
 }
